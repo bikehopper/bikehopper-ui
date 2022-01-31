b844740116d239dc37519d6b8844bb9bf5eea6d4
Andy McCoy <Andykmcc@users.noreply.github.com>
2022-01-31 15:46:59 -0800

    Add leaflet UI (#1)
    Move to custom UI with using API proxy

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-
-
-function GraphhopperProfiles() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BIKEHOPPER_DOMAIN}/v1/info`)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result.profiles);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.name}>
-            {item.name} {item.vehicle}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+import { Map } from './components/map/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <GraphhopperProfiles></GraphhopperProfiles>
-      </header>
+      <div className="map-container">
+        <Map></Map>
+      </div>
     </div>
   );
 }
