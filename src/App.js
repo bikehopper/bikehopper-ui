37b0805bfbd40099a3ed36f2cd9794fb6a5d2abb
Andy McCoy <andy.k.mccoy@gmail.com>
2022-01-30 18:31:58 -0800

    Add example of calling staging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
+function GraphhopperProfiles() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/v1/info")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.profiles);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map(item => (
+          <li key={item.name}>
+            {item.name} {item.vehicle}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 function App() {
   return (
@@ -17,6 +59,7 @@ function App() {
         >
           Learn React
         </a>
+        <GraphhopperProfiles></GraphhopperProfiles>
       </header>
     </div>
   );
