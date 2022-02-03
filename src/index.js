602d8a23a8dfed4b10466f497fedb4eeb45a0d1e
Hazel Court <hcourt@uchicago.edu>
2022-02-02 16:49:25 -0800

    Add search bar

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import MapGL, { Layer, Marker, Source } from 'react-map-gl';
 import { BikeHopperClient } from './lib/bikehopperClient';
 import MarkerSVG from './components/MarkerSVG';
+import SearchBar from './components/SearchBar';
 
 import './index.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
@@ -35,34 +36,60 @@ class Root extends Component {
     };
   }
 
-  _onStartDragEnd(event, end) {
-    this.setState({ points: [event.lngLat, end] });
-    this._redraw();
+  _onStartDragEnd = (event) => {
+    const newPoints = [event.lngLat, this.state.points[1]];
+    this.setState({ points: newPoints });
+    this._route(newPoints);
   }
 
-  _onEndDragEnd(event, start) {
-    this.setState({ points: [start, event.lngLat] });
-    this._redraw();
+  _onEndDragEnd = (event) => {
+    const newPoints = [this.state.points[0], event.lngLat];
+    this.setState({ points: newPoints });
+    this._route(newPoints);
   }
 
   _handleViewportChange = (viewport) => {
     this.setState({ viewport });
   }
 
-  _redraw() {
+  _handlePointSearch = (searchString) => {
+    let point = searchString.split(',')?.slice(0, 2)?.map(s => s.trim());
+
+    if (!point || !point.length) return;
+
+    for (const i in point) {
+      if (isNaN(Number(point[i]))) return;
+      point[i] = Number(point[i]);
+    }
+
+    const newPoints = [this.state.points[0], point];
+    this.setState({ points: newPoints })
+    this._route(newPoints);
+  }
+
+  _route(points) {
     bikeHopperClient.getRoute({
-      points: this.state.points.map(crd => crd.slice(0, 2).reverse()),
+      points: points.map(crd => crd.slice(0, 2).reverse()),
       optimize: true,
       pointsEncoded: false,
     })
       .then(route => {
-        this.setState({ lines: route.paths.map(path => path.points.coordinates) });
-      });
+        this.setState({
+          lines: route.paths.map(path => path.points.coordinates),
+          viewport: {
+            latitude: (points[0][1] + points[1][1]) / 2,
+            longitude: (points[0][0] + points[1][0]) / 2,
+            zoom: 12,
+            bearing: 0,
+            pitch: 0
+          }
+        });
+      })
   }
 
   render() {
     const layerStyle = {
-      id: 'point',
+      id: 'line',
       type: 'line',
       paint: {
         'line-width': 3,
@@ -70,29 +97,32 @@ class Root extends Component {
       }
     };
     return (
-      <MapGL
-        {...this.state.viewport}
-        width="100vw"
-        height="100vh"
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        onViewportChange={this._handleViewportChange}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-      >
-        {this.state.lines && <Source type="geojson" data={turf.lineString(this.state.lines[0])}>
-          <Layer {...layerStyle} />
-        </Source>}
-        <Marker id='startMarker' longitude={this.state.points[0][0]} latitude={this.state.points[0][1]} draggable={true} onDragEnd={(event) => this._onStartDragEnd(event, this.state.points[1])} offsetLeft={-13} offsetTop={-39}>
-          <MarkerSVG />
-        </Marker>
-        <Marker id='endMarker' longitude={this.state.points[1][0]} latitude={this.state.points[1][1]} draggable={true} onDragEnd={(event) => this._onEndDragEnd(event, this.state.points[0])} offsetLeft={-13} offsetTop={-39}>
-          <MarkerSVG />
-        </Marker>
-      </MapGL >
+      <div>
+        <SearchBar onSubmit={this._handlePointSearch} position='absolute' />
+        <MapGL
+          {...this.state.viewport}
+          width="100vw"
+          height="100vh"
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          onViewportChange={this._handleViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        >
+          {this.state.lines && <Source type="geojson" data={turf.lineString(this.state.lines[0])}>
+            <Layer {...layerStyle} />
+          </Source>}
+          <Marker id='startMarker' longitude={this.state.points[0][0]} latitude={this.state.points[0][1]} draggable={true} onDragEnd={this._onStartDragEnd} offsetLeft={-13} offsetTop={-39}>
+            <MarkerSVG />
+          </Marker>
+          <Marker id='endMarker' longitude={this.state.points[1][0]} latitude={this.state.points[1][1]} draggable={true} onDragEnd={this._onEndDragEnd} offsetLeft={-13} offsetTop={-39}>
+            <MarkerSVG />
+          </Marker>
+        </MapGL ></div>
+
     );
   }
 
   componentDidMount() {
-    this._redraw();
+    this._route(this.state.points);
   }
 }
 
