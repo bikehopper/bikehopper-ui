61ee5a7f62b1da89d9eec5ca09011e6314c2f593
Hazel Court <hcourt@uchicago.edu>
2022-02-09 11:52:01 -0800

    Better search bar

--- src/components/App.js
+++ src/components/App.js
@@ -9,34 +9,38 @@ function App() {
   const [endPoint, setEndPoint] = useState(null);
   const [route, setRoute] = useState(null);
 
-  const handleSearch = (searchString, setFunc) => {
-    if (searchString.match(/^\s*-?\d*\.\d*\s*,\s*-?\d*\.\d*\s*$/)) {
-      // Looks like we were given a lon-lat pair, e.g. -122.4, 37.8
-      let point = searchString.split(',')?.slice(0, 2)?.map(s => s.trim());
+  const _handlePointSearch = (searchString, setFunc) => {
+    if (!searchString.match(/^\s*-?\d*\.\d*\s*,\s*-?\d*\.\d*\s*$/)) return false;
+    // Looks like we were given a lon-lat pair, e.g. -122.4, 37.8
+    let point = searchString.split(',')?.slice(0, 2)?.map(s => s.trim());
 
-      if (!point || !point.length) return;
+    if (!point || !point.length) return false;
 
-      for (const i in point) {
-        if (isNaN(Number(point[i]))) return;
-        point[i] = Number(point[i]);
-      }
-      setFunc(point);
-    } else {
-      // It doesn't look like a lon-lat pair. Probably address or place name. Geocode it.
-      const opts = {
-        // XXX oops, I want to use the viewport of the map here, but I put that state in
-        // a subcomponent...
-        lon: -122.4,
-        lat: 37.8,
-      };
-      geocode(searchString, opts).then(result => {
-        if (result.type !== 'FeatureCollection' || result.features[0].geometry.type !== 'Point') {
-          // TODO: show error message (or maybe try to use results that are not points, somehow)
-          return;
-        }
-        setFunc(result.features[0].geometry.coordinates);
-      });
+    for (const i in point) {
+      if (isNaN(Number(point[i]))) return false;
+      point[i] = Number(point[i]);
     }
+    setFunc(point);
+    return true;
+  }
+  const _handleGeocodeSearch = (searchString, setFunc) => {
+    const opts = {
+      // XXX oops, I want to use the viewport of the map here, but I put that state in
+      // a subcomponent...
+      lon: -122.4,
+      lat: 37.8,
+    };
+    geocode(searchString, opts).then(result => {
+      if (result.type !== 'FeatureCollection' || result.features[0].geometry.type !== 'Point') {
+        // TODO: show error message (or maybe try to use results that are not points, somehow)
+        return;
+      }
+      setFunc(result.features[0].geometry.coordinates);
+    });
+  }
+  const handleSearch = ({ start, end }) => {
+    _handlePointSearch(start, setStartPoint) || _handleGeocodeSearch(start, setStartPoint);
+    _handlePointSearch(end, setEndPoint) || _handleGeocodeSearch(end, setEndPoint);
   };
 
   const fetchRoute = () => {
@@ -70,9 +74,7 @@ function App() {
 
   return (
     <div>
-      <SearchBar onSubmit={(s) => handleSearch(s, setStartPoint)} placeholder='Start (enter longitude, latitude)' position='absolute' />
-      <p>to</p>
-      <SearchBar onSubmit={(s) => handleSearch(s, setEndPoint)} placeholder='End (enter longitude, latitude)' position='absolute' />
+      <SearchBar onSubmit={handleSearch} placeholder='End (enter longitude, latitude)' position='absolute' />
       <BikehopperMap
         startPoint={startPoint}
         endPoint={endPoint}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,17 +4,21 @@ export default class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
+      start: undefined,
+      end: undefined,
     };
     this.placeholder = props.placeholder;
   }
 
-  _handleChange = (event) => {
-    this.setState({ value: event.target.value });
+  _setStart = (event) => {
+    this.setState({ start: event.target.value });
+  }
+  _setEnd = (event) => {
+    this.setState({ end: event.target.value });
   }
   _handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.value)
+    this.props.onSubmit(this.state)
   }
 
   render() {
@@ -23,9 +27,10 @@ export default class SearchBar extends React.Component {
     };
     return (
       <form onSubmit={this._handleSubmit}>
-        <label>
-          <input style={style} type='text' placeholder={this.placeholder} onChange={this._handleChange} />
-        </label>
+        <label for='start'>Start</label>
+        <input style={style} id='start' type='text' autofocus required placeholder='from' onChange={this._setStart} />
+        <label for='end'>End</label>
+        <input style={style} id='end' type='text' required placeholder='to' onChange={this._setEnd} />
         <input type="submit" value="Submit" />
       </form>
     );
