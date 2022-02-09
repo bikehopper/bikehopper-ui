9d813d5f258ecaf031ff65390018d09be5f96921
Arindam Bose <arindam.1993@gmail.com>
2022-02-08 16:41:57 -0800

    hitting the pt route (#6)

--- src/lib/BikehopperClient.js
+++ src/lib/BikehopperClient.js
@@ -1,11 +1,11 @@
 export async function getRoute({
-  profile = 'bike2',
+  profile = 'pt',
   optimize = false,
   pointsEncoded = false,
   points,
   signal,
 }) {
-  const route = await fetch(`${process.env.REACT_APP_BIKEHOPPER_DOMAIN}/v1/route?point=${points[0]}&point=${points[1]}&profile=${profile}&optimize=${optimize}&points_encoded=${pointsEncoded}`, {
+  const route = await fetch(`${process.env.REACT_APP_BIKEHOPPER_DOMAIN}/v1/route-pt?point=${points[0]}&point=${points[1]}&locale=en-US&pt.earliest_departure_time=${encodeURIComponent(new Date().toISOString())}&elevation=true&profile=${profile}&use_miles=false&selected_detail=Elevation&layer=OpenStreetMap&points_encoded=${pointsEncoded}`, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
     signal,
