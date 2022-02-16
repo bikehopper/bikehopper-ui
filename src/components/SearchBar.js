5a714e68dc14d0125c5834c2e009544f4ca5ce79
Scott Feeney <scott@oceanbase.org>
2022-02-16 15:46:55 -0800

    run prettier

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,25 +11,41 @@ export default class SearchBar extends React.Component {
 
   _setStart = (event) => {
     this.setState({ start: event.target.value });
-  }
+  };
   _setEnd = (event) => {
     this.setState({ end: event.target.value });
-  }
+  };
   _handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state)
-  }
+    this.props.onSubmit(this.state);
+  };
 
   render() {
     const style = {
-      width: "20rem", background: "#F2F1F9", border: "none", padding: "0.5rem"
+      width: '20rem',
+      background: '#F2F1F9',
+      border: 'none',
+      padding: '0.5rem',
     };
     return (
       <form onSubmit={this._handleSubmit}>
         <label>Start</label>
-        <input style={style} type='text' autoFocus required placeholder='from' onChange={this._setStart} />
+        <input
+          style={style}
+          type="text"
+          autoFocus
+          required
+          placeholder="from"
+          onChange={this._setStart}
+        />
         <label>End</label>
-        <input style={style} type='text' required placeholder='to' onChange={this._setEnd} />
+        <input
+          style={style}
+          type="text"
+          required
+          placeholder="to"
+          onChange={this._setEnd}
+        />
         <input type="submit" value="Submit" />
       </form>
     );
