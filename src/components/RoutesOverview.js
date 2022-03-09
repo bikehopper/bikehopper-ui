2e0c8e7184cf661140881c104fb88b3e3f165550
Scott Feeney <scott@oceanbase.org>
2022-03-08 23:36:08 -0800

    remove needless div from RoutesOverview

--- src/components/RoutesOverview.js
+++ src/components/RoutesOverview.js
@@ -25,49 +25,46 @@ export default function RoutesOverview(props) {
   };
 
   return (
-    <div className="RoutesOverview">
-      <ul className="RoutesOverview_list">
-        {routes.map((route, index) => (
-          <li
-            className={classnames({
-              RoutesOverview_route: true,
-              RoutesOverview_routeSelected: activeRoute === index,
-            })}
-            onClick={handleRouteClick.bind(null, index)}
-          >
-            <ul className="RoutesOverview_routeLegs">
-              {route.legs.map((leg, index) => (
-                <>
-                  {index > 0 && (
-                    <li className="RoutesOverview_legSeparator">
-                      <Icon>
-                        <NavArrowRight />
-                      </Icon>
-                    </li>
-                  )}
-                  <li className="RoutesOverview_leg">
-                    <RouteLeg
-                      type={leg.type}
-                      routeName={leg.route_name}
-                      routeColor={leg.route_color}
-                      agencyName={leg.agency_name}
-                      duration={
-                        /* hide duration if route has only one leg */
-                        route.legs.length > 1 &&
-                        new Date(leg.arrival_time) -
-                          new Date(leg.departure_time)
-                      }
-                    />
+    <ul className="RoutesOverview">
+      {routes.map((route, index) => (
+        <li
+          className={classnames({
+            RoutesOverview_route: true,
+            RoutesOverview_routeSelected: activeRoute === index,
+          })}
+          onClick={handleRouteClick.bind(null, index)}
+        >
+          <ul className="RoutesOverview_routeLegs">
+            {route.legs.map((leg, index) => (
+              <>
+                {index > 0 && (
+                  <li className="RoutesOverview_legSeparator">
+                    <Icon>
+                      <NavArrowRight />
+                    </Icon>
                   </li>
-                </>
-              ))}
-            </ul>
-            <p className="RoutesOverview_timeEstimate">
-              {formatInterval(route.time)}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </div>
+                )}
+                <li className="RoutesOverview_leg">
+                  <RouteLeg
+                    type={leg.type}
+                    routeName={leg.route_name}
+                    routeColor={leg.route_color}
+                    agencyName={leg.agency_name}
+                    duration={
+                      /* hide duration if route has only one leg */
+                      route.legs.length > 1 &&
+                      new Date(leg.arrival_time) - new Date(leg.departure_time)
+                    }
+                  />
+                </li>
+              </>
+            ))}
+          </ul>
+          <p className="RoutesOverview_timeEstimate">
+            {formatInterval(route.time)}
+          </p>
+        </li>
+      ))}
+    </ul>
   );
 }
