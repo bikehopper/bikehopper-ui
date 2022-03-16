df35525052416958f369f8d2f87797be6dc571c3
Scott Feeney <scott@oceanbase.org>
2022-03-16 16:59:52 -0700

    make clickable SelectionListItem s focusable
    I need this so the SearchAutocompleteDropdown results can be clickable,
    while still hiding the autocomplete results if you focus something
    outside of SearchBar and its descendants.
    
    This is also an accessibility improvement, compared to having a list
    item (<li>) with an onClick. However, it actually makes an accessibility
    lint warning complain, because technically <button> is more semantic
    than <a>. While this is true, resetting browser default styles for
    <button> is a pain in the butt, and I don't have the energy to take it
    on right now.

--- src/components/SelectionListItem.js
+++ src/components/SelectionListItem.js
@@ -4,17 +4,41 @@ import classnames from 'classnames';
 import './SelectionListItem.css';
 
 export default function SelectionListItem(props) {
+  const clickable = !!props.onClick;
+
+  const handleClick = (evt) => {
+    evt.preventDefault();
+    props.onClick(evt);
+  };
+
+  /* eslint-disable jsx-a11y/anchor-is-valid */
+  // (The fix for this rule is to make this a <button>, but then it's a huge
+  // pain to remove all the default CSS.)
+  const contents = props.onClick ? (
+    <a
+      href="#"
+      onClick={handleClick}
+      className={classnames({
+        SelectionListItem_link: true,
+        [props.className]: !!props.className,
+      })}
+    >
+      {props.children}
+    </a>
+  ) : (
+    props.children
+  );
+
   return (
     <li
       className={classnames({
         SelectionListItem: true,
         SelectionListItem_active: props.active,
-        SelectionListItem_clickable: !!props.onClick,
-        [props.className]: !!props.className,
+        SelectionListItem_hasLinkChild: clickable,
+        [props.className]: !clickable && !!props.className,
       })}
-      onClick={props.onClick}
     >
-      {props.children}
+      {contents}
     </li>
   );
 }
