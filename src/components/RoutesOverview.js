4838df75775b336569eedfde0b52772a54ed75ed
Scott Feeney <scott@oceanbase.org>
2022-03-22 15:07:55 -0700

    add departure/arrival times

--- src/components/RoutesOverview.js
+++ src/components/RoutesOverview.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { routeClicked } from '../features/routes';
 import { formatInterval } from '../lib/time';
+import DepartArriveTime from './DepartArriveTime';
 import Icon from './Icon';
 import RouteLeg from './RouteLeg';
 import SelectionList from './SelectionList';
@@ -34,35 +35,43 @@ export default function RoutesOverview(props) {
           onClick={handleRouteClick.bind(null, index)}
           key={route.nonce}
         >
-          <ul className="RoutesOverview_routeLegs">
-            {route.legs.map((leg, index) => (
-              <React.Fragment key={route.nonce + ':' + index}>
-                {index > 0 && (
-                  <li className="RoutesOverview_legSeparator">
-                    <Icon>
-                      <NavArrowRight />
-                    </Icon>
+          <div className="RoutesOverview_row">
+            <ul className="RoutesOverview_routeLegs">
+              {route.legs.map((leg, index) => (
+                <React.Fragment key={route.nonce + ':' + index}>
+                  {index > 0 && (
+                    <li className="RoutesOverview_legSeparator">
+                      <Icon>
+                        <NavArrowRight />
+                      </Icon>
+                    </li>
+                  )}
+                  <li className="RoutesOverview_leg">
+                    <RouteLeg
+                      type={leg.type}
+                      routeName={leg.route_name}
+                      routeColor={leg.route_color}
+                      agencyName={leg.agency_name}
+                      duration={
+                        /* hide duration if route has only one leg */
+                        route.legs.length > 1 &&
+                        new Date(leg.arrival_time) -
+                          new Date(leg.departure_time)
+                      }
+                    />
                   </li>
-                )}
-                <li className="RoutesOverview_leg">
-                  <RouteLeg
-                    type={leg.type}
-                    routeName={leg.route_name}
-                    routeColor={leg.route_color}
-                    agencyName={leg.agency_name}
-                    duration={
-                      /* hide duration if route has only one leg */
-                      route.legs.length > 1 &&
-                      new Date(leg.arrival_time) - new Date(leg.departure_time)
-                    }
-                  />
-                </li>
-              </React.Fragment>
-            ))}
-          </ul>
-          <p className="RoutesOverview_timeEstimate">
-            {formatInterval(route.time)}
-          </p>
+                </React.Fragment>
+              ))}
+            </ul>
+            <p className="RoutesOverview_timeEstimate">
+              {formatInterval(route.time)}
+            </p>
+          </div>
+          <DepartArriveTime
+            className="RoutesOverview_departArriveTime"
+            depart={route.legs[0].departure_time}
+            arrive={route.legs[route.legs.length - 1].arrival_time}
+          />
         </SelectionListItem>
       ))}
     </SelectionList>
