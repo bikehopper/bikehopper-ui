598c975bfd6d240a76bee7223643ad6f7f30c4bf
Scott Feeney <scott@oceanbase.org>
2022-03-29 14:13:18 -0700

    move padding, bg color into TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import SearchBar from './SearchBar';
 
+import './TopBar.css';
+
 export default function TopBar(props) {
-  return <SearchBar />;
+  return (
+    <div className="TopBar">
+      <SearchBar />
+    </div>
+  );
 }
