10d229cc3d48b9f4162bba178afb5566a39ffab6
Scott Feeney <scott@oceanbase.org>
2022-04-23 23:10:57 -0700

    don't shout AC TRANSIT

--- src/lib/getAgencyNameForDisplay.js
+++ src/lib/getAgencyNameForDisplay.js
@@ -1,6 +1,7 @@
 const COMMON_NAMES = {
   'San Francisco Municipal Transportation Agency': 'Muni',
   'Bay Area Rapid Transit': 'BART',
+  'AC TRANSIT': 'AC Transit',
 };
 
 export default function getAgencyNameForDisplay(fullName) {
