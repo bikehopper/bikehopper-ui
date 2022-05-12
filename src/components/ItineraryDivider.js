ded0f8f0f77e11b7de6dc4bd21627f6d24a2bcba
glorialiu <gloriafire96@gmail.com>
2022-05-11 18:27:28 -0700

    update classnames

--- src/components/ItineraryDivider.js
+++ src/components/ItineraryDivider.js
@@ -1,18 +1,25 @@
 import * as React from 'react';
+import classnames from 'classnames';
 import ItineraryRow from './ItineraryRow';
 
 import './ItineraryDivider.css';
 
 export default function ItineraryDivider(props) {
   const { transit, detail } = props;
-  const dividerType = transit ? 'transit' : 'bike';
+
   return (
     <ItineraryRow>
       <span className="ItineraryDivider_border" />
-      <span className={'ItineraryDivider_content_' + dividerType}>
+      <span
+        className={classnames({
+          ItineraryDivider_subheading: true,
+          ItineraryDivider_subheadingBike: !transit,
+          ItineraryDivider_subheadingTransit: transit,
+        })}
+      >
         {props.children}
       </span>
-      <span className={'ItineraryDivider_detail_' + dividerType}>{detail}</span>
+      <span className={'ItineraryDivider_detail'}>{detail}</span>
     </ItineraryRow>
   );
 }
