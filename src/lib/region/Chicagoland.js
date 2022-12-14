b4fef377e504cfd0d659bd7d283d76660b412f0e
Scott Feeney <scott@oceanbase.org>
2022-12-14 00:37:17 -0600

    some Chicago values for demo

---
+++ src/lib/region/Chicagoland.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const SUPPORTED_REGION_DISPLAY = (
+  <span>
+    the <strong>Chicago</strong> area
+  </span>
+);
+
+export const DEFAULT_VIEWPORT_BOUNDS = [-89.057, 40.937, -86.255, 42.932];
+
+export const AGENCY_COMMON_NAMES = {
+  'Chicago Transit Authority': 'CTA',
+};
