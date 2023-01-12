7918ffe0ee8bb0b8177495377a9f3f833565b373
Scott Feeney <scott@oceanbase.org>
2023-01-11 18:04:34 -0800

    Use custom place icons in search bar
    Status quo: If we have a relevant custom icon for a feature (e.g.
    steaming coffee mug for a cafe, tree icon for a park), we display that
    icon in the autocomplete drop down instead of a pin. But we still always
    use the generic pin icon in the search bar itself.
    
    This commit: Uses the custom icon, if any, in the search bar itself for
    geocoded features. It will be replaced with a generic pin icon if the
    location text is subsequently edited.
    
    Limitation of this commit: If the location is hydrated from the URL
    (reload page or follow link), the custom icon will be lost because in
    that case we only have the place name and coordinates, not further
    details.

--- src/components/SearchAutocompleteDropdown.js
+++ src/components/SearchAutocompleteDropdown.js
@@ -8,28 +8,11 @@ import {
 } from '../features/routeParams';
 import describePlace from '../lib/describePlace';
 import Icon from './Icon';
+import PlaceIcon from './PlaceIcon';
 import SelectionList from './SelectionList';
 import SelectionListItem from './SelectionListItem';
-import { ReactComponent as Building } from 'iconoir/icons/building.svg';
-import { ReactComponent as Chocolate } from 'iconoir/icons/chocolate.svg';
-import { ReactComponent as CoffeeCup } from 'iconoir/icons/coffee-cup.svg';
-import { ReactComponent as Cutlery } from 'iconoir/icons/clutery.svg';
-import { ReactComponent as Cycling } from 'iconoir/icons/cycling.svg';
-import { ReactComponent as Flower } from 'iconoir/icons/flower.svg';
-import { ReactComponent as GlassHalf } from 'iconoir/icons/glass-half.svg';
-import { ReactComponent as Golf } from 'iconoir/icons/golf.svg';
-import { ReactComponent as Gym } from 'iconoir/icons/gym.svg';
-import { ReactComponent as Home } from 'iconoir/icons/home.svg';
-import { ReactComponent as Hospital } from 'iconoir/icons/hospital.svg';
-import { ReactComponent as PharmacyCrossCircle } from 'iconoir/icons/pharmacy-cross-circle.svg';
-import { ReactComponent as Pin } from 'iconoir/icons/pin-alt.svg';
-import { ReactComponent as PineTree } from 'iconoir/icons/pine-tree.svg';
+
 import { ReactComponent as Position } from 'iconoir/icons/position.svg';
-import { ReactComponent as Sandals } from 'iconoir/icons/sandals.svg';
-import { ReactComponent as Shop } from 'iconoir/icons/shop.svg';
-import { ReactComponent as StarOutline } from 'iconoir/icons/star.svg';
-import { ReactComponent as Swimming } from 'iconoir/icons/swimming.svg';
-import { ReactComponent as Trekking } from 'iconoir/icons/trekking.svg';
 
 import './SearchAutocompleteDropdown.css';
 
@@ -134,9 +117,10 @@ export default function SearchAutocompleteDropdown(props) {
           key={feature.properties.osm_id + ':' + feature.properties.type}
           onClick={handleClick.bind(null, index)}
         >
-          <Icon className="SearchAutocompleteDropdown_icon">
-            {_getSvgForFeature(feature)}
-          </Icon>
+          <PlaceIcon
+            className="SearchAutocompleteDropdown_icon"
+            place={feature}
+          />
           <span className="SearchAutocompleteDropdown_placeDescription">
             {describePlace(feature)}
           </span>
@@ -150,76 +134,4 @@ export default function SearchAutocompleteDropdown(props) {
 export function isAutocompleteResultElement(domElement) {
   if (!domElement) return false;
   return Array.from(domElement.classList).includes(LIST_ITEM_CLASSNAME);
-}
-
-function _getSvgForFeature(feature) {
-  const { osm_key: key, osm_value: value, type } = feature?.properties || {};
-
-  let Klass = Pin;
-
-  if (
-    (key === 'boundary' && value === 'national_park') ||
-    (key === 'leisure' && ['park', 'nature_reserve'].includes(value))
-  ) {
-    // park
-    Klass = PineTree;
-  } else if (
-    (key === 'natural' && ['beach', 'shingle'].includes(value)) ||
-    (key === 'leisure' && value === 'beach_resort')
-  ) {
-    // beach
-    Klass = Sandals;
-  } else if (
-    (key === 'natural' && ['peak', 'hill', 'rock', 'saddle'].includes(value)) ||
-    (key === 'highway' && value === 'footway' && type === 'street')
-  ) {
-    // mountain, trail
-    Klass = Trekking;
-  } else if (key === 'leisure' && value === 'garden') {
-    Klass = Flower;
-  } else if (key === 'leisure' && value === 'golf_course') {
-    Klass = Golf;
-  } else if (
-    key === 'leisure' &&
-    ['swimming_area', 'swimming_pool', 'water_park'].includes(value)
-  ) {
-    Klass = Swimming;
-  } else if (key === 'tourism' && value === 'attraction') {
-    Klass = StarOutline;
-  } else if (key === 'amenity' && value === 'cafe') {
-    Klass = CoffeeCup;
-  } else if (
-    key === 'amenity' &&
-    ['restaurant', 'fast_food', 'food_court'].includes(value)
-  ) {
-    Klass = Cutlery;
-  } else if (
-    key === 'amenity' &&
-    ['bar', 'biergarten', 'pub'].includes(value)
-  ) {
-    Klass = GlassHalf;
-  } else if (key === 'place' && value === 'house') {
-    // note: we can't rely on type === 'house', shops have that
-    Klass = Home;
-  } else if (key === 'shop' && value === 'chocolate') {
-    Klass = Chocolate;
-  } else if (
-    (key === 'amenity' && value === 'pharmacy') ||
-    (key === 'shop' && value === 'chemist')
-  ) {
-    Klass = PharmacyCrossCircle;
-  } else if (key === 'amenity' && value === 'hospital') {
-    Klass = Hospital;
-  } else if (key === 'highway' && value === 'cycleway') {
-    Klass = Cycling;
-  } else if (key === 'leisure' && value === 'fitness_centre') {
-    Klass = Gym;
-  } else if (key === 'shop') {
-    // fallback for other types of shops than above
-    Klass = Shop;
-  } else if (key === 'amenity' && value === 'townhall') {
-    Klass = Building; // might not be a great fit for city halls, but best I can do
-  }
-
-  return <Klass />;
 }
