96dc85d8d75c73a846a92466dfa68241e608199c
Scott Feeney <scott@oceanbase.org>
2023-05-06 22:11:45 -0700

    basic, crummy looking select

--- src/components/TimeBar.jsx
+++ src/components/TimeBar.jsx
@@ -1,16 +1,19 @@
 import Bowser from 'bowser';
 import { DateTime } from 'luxon';
 import * as React from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Transition } from '@headlessui/react';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as Select from '@radix-ui/react-select';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import Icon from './Icon';
 
 import { initialTimeSet, departureTypeSelected } from '../features/routeParams';
 
 import { ReactComponent as CancelIcon } from 'iconoir/icons/cancel.svg';
+import { ReactComponent as CheckIcon } from 'iconoir/icons/check.svg';
 import { ReactComponent as ClockOutline } from 'iconoir/icons/clock.svg';
+import { ReactComponent as NavArrowDown } from 'iconoir/icons/nav-arrow-down.svg';
 import './TimeBar.css';
 
 // contract:
@@ -18,14 +21,14 @@ import './TimeBar.css';
 // modify by firing departureTypeSelected and initialTimeSet actions
 
 export default function TimeBar(props) {
-  const { departureType, timeFromGlobalState } = useSelector(
+  const { globalDepartureType, globalInitialTime } = useSelector(
     ({ routeParams }) => {
       let departureType = 'departAt';
       if (routeParams.arriveBy) departureType = 'arriveBy';
       else if (routeParams.initialTime == null) departureType = 'now';
       return {
-        departureType,
-        timeFromGlobalState: routeParams.initialTime,
+        globalDepartureType: departureType,
+        globalInitialTime: routeParams.initialTime,
       };
     },
     shallowEqual,
@@ -35,24 +38,52 @@ export default function TimeBar(props) {
   const intl = useIntl();
 
   // From the time in global state, generate strings for the date & time <input>s
-  const datetime = DateTime.fromMillis(timeFromGlobalState || Date.now());
-  const timeForTimeInput = datetime.toFormat('HH:mm');
-  const dateForDateInput = datetime.toFormat('yyyy-MM-dd');
+  const datetime = DateTime.fromMillis(globalInitialTime || Date.now());
+  const globalTime = datetime.toFormat('HH:mm');
+  const globalDate = datetime.toFormat('yyyy-MM-dd');
 
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
 
-  const handleUpdateClick = React.useCallback((evt) => {
-    console.log('update clicked');
+  // Local state for the departure type, date and time, as it is being actively edited
+  // in the dialog, and has not yet been committed to global state.
+  const [pendingDepartureType, setPendingDepartureType] =
+    React.useState(globalDepartureType);
+  const [pendingDate, setPendingDate] = React.useState(globalDate);
+  const [pendingTime, setPendingTime] = React.useState(globalTime);
+
+  const handleUpdateClick = (evt) => {
+    if (pendingDepartureType !== globalDepartureType)
+      dispatch(departureTypeSelected(pendingDepartureType));
+    if (pendingDate !== globalDate || pendingTime !== globalTime) {
+      dispatch(
+        initialTimeSet(
+          DateTime.fromFormat(
+            globalDate + ' ' + globalTime,
+            'yyyy-MM-dd HH:mm',
+          ).toMillis(),
+        ),
+      );
+    }
     setIsDialogOpen(false);
-  }, []);
+  };
+
+  const handleDialogOpenChange = (isOpen) => {
+    if (isOpen) {
+      // Initialize the local (pending) state to match the global state.
+      setPendingDepartureType(globalDepartureType);
+      setPendingDate(globalDate);
+      setPendingTime(globalTime);
+    }
+    setIsDialogOpen(isOpen);
+  };
 
   return (
     <div className="TimeBar">
-      <span>{departureType + ' '}</span>
-      {departureType !== 'now' && (
-        <span>{dateForDateInput + ' ' + timeForTimeInput}</span>
+      <span>{globalDepartureType + ' '}</span>
+      {globalDepartureType !== 'now' && (
+        <span>{globalDate + ' ' + globalTime}</span>
       )}
-      <Dialog.Root open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <Dialog.Root open={isDialogOpen} onOpenChange={handleDialogOpenChange}>
         <Dialog.Trigger asChild>
           <button>Change</button>
         </Dialog.Trigger>
@@ -89,7 +120,72 @@ export default function TimeBar(props) {
                   bg-white p-6 rounded-md"
               >
                 <Dialog.Title className="m-0">Change trip time</Dialog.Title>
-                <p>controls to come here</p>
+                <Select.Root
+                  value={pendingDepartureType}
+                  onValueChange={setPendingDepartureType}
+                >
+                  <Select.Trigger className="">
+                    <Select.Value />
+                    <Select.Icon>
+                      <Icon>
+                        <NavArrowDown width="20" height="20" />
+                      </Icon>
+                    </Select.Icon>
+                  </Select.Trigger>
+                  <Select.Portal>
+                    <Select.Content position="popper" className="z-30 bg-white">
+                      <Select.Viewport>
+                        <Select.Item value="now">
+                          <Select.ItemText>
+                            <FormattedMessage
+                              defaultMessage="Now"
+                              description="option in a dropdown list for departure time"
+                            />
+                          </Select.ItemText>
+                          <Select.ItemIndicator>
+                            <Icon>
+                              <CheckIcon width="20" height="20" />
+                            </Icon>
+                          </Select.ItemIndicator>
+                        </Select.Item>
+                        <Select.Item value="departAt">
+                          <Select.ItemText>
+                            <FormattedMessage
+                              defaultMessage="Depart at"
+                              description={
+                                'option in a dropdown list for departure time.' +
+                                ' There is another input next to it to select' +
+                                ' the time to depart at.'
+                              }
+                            />
+                          </Select.ItemText>
+                          <Select.ItemIndicator>
+                            <Icon>
+                              <CheckIcon width="20" height="20" />
+                            </Icon>
+                          </Select.ItemIndicator>
+                        </Select.Item>
+                        <Select.Item value="arriveBy">
+                          <Select.ItemText>
+                            <FormattedMessage
+                              defaultMessage="Arrive by"
+                              description={
+                                'option in a dropdown list for departure time.' +
+                                ' There is another input next to it to select' +
+                                ' the time to arrive by.'
+                              }
+                            />
+                          </Select.ItemText>
+                          <Select.ItemIndicator>
+                            <Icon>
+                              <CheckIcon width="20" height="20" />
+                            </Icon>
+                          </Select.ItemIndicator>
+                        </Select.Item>
+                      </Select.Viewport>
+                    </Select.Content>
+                  </Select.Portal>
+                </Select.Root>
                 <button onClick={handleUpdateClick} className="">
                   Update
                 </button>
@@ -113,32 +209,4 @@ export default function TimeBar(props) {
       </Dialog.Root>
     </div>
   );
-
-  const options = [
-    {
-      value: 'now',
-      label: intl.formatMessage({
-        defaultMessage: 'Now',
-        description: 'option in a dropdown list for departure time',
-      }),
-    },
-    {
-      value: 'departAt',
-      label: intl.formatMessage({
-        defaultMessage: 'Depart at',
-        description:
-          'option in a dropdown list for departure time.' +
-          ' There is another input next to it to select the time to depart at.',
-      }),
-    },
-    {
-      value: 'arriveBy',
-      label: intl.formatMessage({
-        defaultMessage: 'Arrive by',
-        description:
-          'option in a dropdown list for departure time.' +
-          ' There is another input next to it to select the time to arrive by.',
-      }),
-    },
-  ];
 }
