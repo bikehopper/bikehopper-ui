bikehopper/bikehopper-ui

commit 02ad54cdaea69a94e29ba5da5333fbd4c9109052

Navigate from current loc to loc chosen on map
Fixes #272

src/features/routeParams.js
@@ -67,6 +67,14 @@ export function routeParamsReducer(state = DEFAULT_STATE, action) {
           source: LocationSourceType.SelectedOnMap,
         };
         draft[action.startOrEnd + 'InputText'] = '';
+        if (state.start == null) {
+          // if no start point, and "directions to" was selected,
+          // route from current location.
+          draft.start = {
+            point: null,
+            source: LocationSourceType.UserGeolocation,
+          };
+        }
       });
     case 'params_hydrated_from_url':
       return produce(state, (draft) => {
@@ -376,14 +384,34 @@ export function locationSelectedOnMap(startOrEnd, coords) {
         initialTime,
         _computeBlockRouteTypes(connectingModes),
       )(dispatch, getState);
-    } else if (startOrEnd === 'end' && start?.point?.geometry.coordinates) {
-      await fetchRoute(
-        start.point.geometry.coordinates,
-        coords,
-        arriveBy,
-        initialTime,
-        _computeBlockRouteTypes(connectingModes),
-      )(dispatch, getState);
+    } else if (startOrEnd === 'end' && start != null) {
+      // Potentially geolocate user for start point
+      if (start?.source === LocationSourceType.UserGeolocation) {
+        await dispatch(geolocate());
+        const { lng, lat } = getState().geolocation;
+        if (lng == null || lat == null) return;
+
+        start = {
+          point: turf.point([lng, lat]),
+          source: LocationSourceType.UserGeolocation,
+        };
+
+        await dispatch({
+          type: 'locations_set',
+          start,
+          end,
+        });
+      }
+
+      if (start?.point?.geometry.coordinates) {
+        await fetchRoute(
+          start.point.geometry.coordinates,
+          coords,
+          arriveBy,
+          initialTime,
+          _computeBlockRouteTypes(connectingModes),
+        )(dispatch, getState);
+      }
     }
   };
 }

commit d9deaa79af544fd0e0a1826f22c957fb52401f27

Fix "directions from"
Broken by 02ad54cdaea6

src/features/routeParams.js
@@ -67,7 +67,7 @@ export function routeParamsReducer(state = DEFAULT_STATE, action) {
           source: LocationSourceType.SelectedOnMap,
         };
         draft[action.startOrEnd + 'InputText'] = '';
-        if (state.start == null) {
+        if (action.startOrEnd === 'end' && state.start == null) {
           // if no start point, and "directions to" was selected,
           // route from current location.
           draft.start = {