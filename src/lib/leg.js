b692326e1c4f4c7cd4e41f811254f84382c55e78
Scott Feeney <scott@oceanbase.org>
2024-04-29 16:16:55 -0700

    re-internalize isSignificantLeg

--- src/lib/leg.js
+++
@@ -1,9 +0,0 @@
-export function isSignificantLeg(leg) {
-  // For filtering out short, interpolated legs
-  const THRESHOLD_IN_METERS = 120;
-  return !(
-    leg.type === 'bike2' &&
-    leg.interpolated &&
-    leg.distance < THRESHOLD_IN_METERS
-  );
-}
