9b11ef119749dc91d0c226cd968837806977600f
Andy McCoy <andy.k.mccoy@gmail.com>
2024-05-20 12:26:04 -0700

    Remove vite-plugin-checker
    The TSC output was overwriting the vite output in the terminal. its not clear to me what this checker even did since everything worked without it.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import babelPluginFormatjs from 'babel-plugin-formatjs';
-import checker from 'vite-plugin-checker';
 
 export default defineConfig(() => {
   return {
@@ -28,9 +27,6 @@ export default defineConfig(() => {
       }),
       eslint2(),
       basicSsl(),
-      checker({
-        typescript: true,
-      }),
     ],
     server: {
       open: true,
