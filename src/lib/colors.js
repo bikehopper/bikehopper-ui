715bda47265b8efba8b3e3188682e4ce9229634a
Scott Feeney <scott@oceanbase.org>
2024-05-29 18:52:23 -0700

    Distinguish bike colors more

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,8 +1,8 @@
 import Color from 'color';
 
-export const DEFAULT_BIKE_COLOR = '#5c5c3d';
+export const DEFAULT_BIKE_COLOR = '#6b6b47';
 export const BIKE_LANE_COLOR = '#33cc33';
-export const CYCLE_TRACK_COLOR = '#006600';
+export const CYCLE_TRACK_COLOR = '#0c8014';
 export const DEFAULT_PT_COLOR = '#4169e1';
 export const DEFAULT_INACTIVE_COLOR = 'darkgray';
 export const BIKEHOPPER_THEME_COLOR = '#5aaa0a';
