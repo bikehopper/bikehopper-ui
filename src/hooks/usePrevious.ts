ae71fff1a48b5ca073a8388cec4aacdd2ef5548a
Scott Feeney <scott@oceanbase.org>
2024-06-03 16:22:04 -0700

    type some hooks

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 
-export default function usePrevious(value) {
-  const ref = useRef();
+export default function usePrevious<T>(value: T) {
+  const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
   }, [value]);
