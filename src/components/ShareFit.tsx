536d4eccad7febfe291df88e5f269d1a7e705d48
Scott Feeney <scott@oceanbase.org>
2024-06-05 00:24:16 -0700

    Code split gpx2fit
    gpx2fit is a relatively large dependency (~300kB uncompressed) that many
    BH users will not use. This uses a dynamic import to defer loading it
    until a FIT file is downloaded.
    
    To show it's doing something, the FIT button grays while the code is
    being downloaded, and if it fails to load, an alert is displayed. I
    tested this by adding a delay and by adding a throw, respectively.

---
+++ src/components/ShareFit.tsx
@@ -0,0 +1,108 @@
+import classnames from 'classnames';
+import { useCallback, useState } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { useDispatch } from 'react-redux';
+import Icon from './primitives/Icon';
+import type { RouteResponsePath } from '../lib/BikehopperClient';
+import { fitFileGenerationFailed } from '../features/misc.js';
+import ModalDialog from './primitives/ModalDialog';
+
+import DownloadIcon from 'iconoir/icons/download.svg?react';
+
+type Props = {
+  route: RouteResponsePath;
+};
+
+export default function ShareFit({ route }: Props) {
+  const intl = useIntl();
+  const dispatch = useDispatch();
+
+  // Data used to query for our fit file.
+  const [fitURL, setFitURL] = useState<string | null>(null);
+
+  const [isDownloading, setIsDownloading] = useState<boolean>(false);
+
+  const leg = route.legs[0];
+
+  // Download the fit file.
+  const downloadFit = useCallback(() => {
+    const downloadFitAsync = async () => {
+      setIsDownloading(true);
+      try {
+        const getFitBlob = (await import('../lib/getFitBlob.js')).default;
+        setFitURL(URL.createObjectURL(getFitBlob(leg)));
+      } catch (err) {
+        console.error('fit file generation error:', err);
+        dispatch(
+          fitFileGenerationFailed(
+            intl.formatMessage({
+              defaultMessage: 'Unable to generate FIT file',
+              description:
+                "error when we can't generate a FIT file " +
+                '(a file format used in a cycling computer).',
+            }),
+          ),
+        );
+      } finally {
+        setIsDownloading(false);
+      }
+    };
+    downloadFitAsync();
+  }, [leg, dispatch, intl]);
+
+  return (
+    <>
+      <button
+        className={classnames({
+          'bg-transparent h-8 block border-0 cursor-pointer text-black': true,
+          'opacity-50': isDownloading,
+        })}
+        onClick={downloadFit}
+        disabled={isDownloading}
+      >
+        <div className="flex items-center mr-8 text-[.80rem]">
+          <Icon
+            label={intl.formatMessage({
+              defaultMessage: 'Download',
+              description: 'Button used to download FIT file.',
+            })}
+          >
+            <DownloadIcon className="align-middle" />
+          </Icon>
+          {intl.formatMessage({
+            defaultMessage: 'FIT',
+            description: 'Button used to download FIT file.',
+          })}
+        </div>
+      </button>
+      <ModalDialog
+        isOpen={fitURL != null}
+        onCancel={() => setFitURL(null)}
+        clickOutsideCancels={true}
+        title={
+          <FormattedMessage
+            defaultMessage="Download FIT file"
+            description={
+              'dialog header. In this dialog you can download' +
+              'FIT file which you can import into your cycling' +
+              'computer.'
+            }
+          />
+        }
+      >
+        {fitURL && (
+          <a
+            download="route.fit"
+            href={fitURL}
+            className="text-blue-500 underline"
+          >
+            {intl.formatMessage({
+              defaultMessage: 'Press here to download the FIT file.',
+              description: 'Link to download the FIT file.',
+            })}
+          </a>
+        )}
+      </ModalDialog>
+    </>
+  );
+}
