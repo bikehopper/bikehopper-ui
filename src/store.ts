0a95098063fca8460e11b8b37e8a5cb6be0f9320
Scott Feeney <scott@oceanbase.org>
2024-06-04 23:42:27 -0700

    Fix linting of TypeScript files (#367)
    `npm run lint` was not scanning ts and tsx files.
    
    This also turns off TS's unused locals/args warning since it is
    duplicative of this more configurable lint rule:
    
    https://typescript-eslint.io/rules/no-unused-vars
    
    I have not turned on the Typescript-ESLint recommended rules yet because
    it produces lots of errors that will need to be dealt with in time.
    
    Co-authored-by: Andy McCoy <andy.k.mccoy@gmail.com>

--- src/store.ts
+++ src/store.ts
@@ -29,7 +29,6 @@ const rootReducer = combineReducers({
 });
 
 declare global {
-  // eslint-disable-next-line no-unused-vars
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
