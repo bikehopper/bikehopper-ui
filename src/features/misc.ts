52d54c455a063caad3f223beb8b4fef47c302606
Scott Feeney <scott@oceanbase.org>
2024-06-05 22:33:47 -0700

    Code split gpx2fit (#369)
    gpx2fit is a relatively large dependency (~300kB uncompressed) that many
    BH users will not use. This uses a dynamic import to defer loading it
    until a FIT file is downloaded.
    
    To show it's doing something, the FIT button grays while the code is
    being downloaded, and if it fails to load, an alert is displayed. I
    tested this by adding a delay and by adding a throw, respectively.
    
    This also rejiggers manual chunks to get more bang for buck.

--- src/features/misc.ts
+++ src/features/misc.ts
@@ -1,8 +1,24 @@
 import type { Action } from 'redux';
+import { ActionAlertMixin, AlertSeverity } from './alerts';
 
 export type MapLoadedAction = Action<'map_loaded'>;
 export function mapLoaded() {
   return { type: 'map_loaded' };
 }
 
-export type MiscAction = MapLoadedAction;
+export type FitFileGenerationFailedAction =
+  Action<'fit_file_generation_failed'>;
+/** This just exists to generate an alert, for now. */
+export function fitFileGenerationFailed(
+  message: string,
+): FitFileGenerationFailedAction & ActionAlertMixin {
+  return {
+    type: 'fit_file_generation_failed',
+    alert: {
+      message,
+      severity: AlertSeverity.ERROR,
+    },
+  };
+}
+
+export type MiscAction = MapLoadedAction | FitFileGenerationFailedAction;
