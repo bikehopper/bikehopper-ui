a6741d92c7b2fc6b116eabace146eeff8fda4107
Scott Feeney <scott@oceanbase.org>
2024-07-24 20:42:18 -0700

    Upgrade dependencies
    Upgrades everything to the latest except for immer, maplibre-gl,
    react-redux, redux, and redux-thunk for the reasons noted in PR #325;
    @headlessui/react because something in 2.x broke BikeHopper and it wasn't
    immediately obvious what to change; eslint out of laziness
    (eslint-plugin-react now supports eslint 9.x, but not sure if we'll have
    to make more changes).

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { shouldPolyfill as listFormatShouldPolyfill } from '@formatjs/intl-listformat/should-polyfill';
-import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 import store from './store';
 
@@ -77,11 +76,6 @@ async function bootstrapApp(): Promise<void> {
       </Provider>
     </StrictMode>,
   );
-
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
 }
 
 bootstrapApp();
