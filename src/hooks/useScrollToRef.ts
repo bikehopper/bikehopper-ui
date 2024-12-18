9a02df00f1a58e8a3c837f94deb8c9947d5a0c3d
Scott Feeney <scott@oceanbase.org>
2024-12-17 22:13:03 -0800

    fix scrolling
    I added position: relative to the main div of ItinerarySingleTransitStop
    to position the X in the top right corner. But this made that div the
    offsetParent, so useScrollToRef tried to scroll within it, instead of
    the actual scroll container (div.MapPlusOverlay_overlay). So I'm
    modifying useScrollToRef to iterate till it finds an actually scrollable
    container.

--- src/hooks/useScrollToRef.ts
+++ src/hooks/useScrollToRef.ts
@@ -2,7 +2,7 @@ import { useRef, useEffect } from 'react';
 import usePrevious from './usePrevious';
 
 // A ref that will be scrolled to when the component mounts, if populated.
-// The offsetParent must be the container in which to scroll.
+// The container in which to scroll must be an offset parent.
 //
 // If a key is passed, changing the key also scrolls to the ref.
 
@@ -21,12 +21,31 @@ export default function useScrollToRef<T extends HTMLElement, K>(
       return;
     }
 
-    const el = scrollToRef.current;
-    if (el) {
-      const container = el.offsetParent;
+    console.log('scrolling to. key=', key);
+    const anchor = scrollToRef.current;
+    if (anchor) {
+      let offsetTop = 0;
+      let el: Element = anchor;
+      let container: Element | null = null;
+
+      // Because anything positioned relative is an offset parent, we may have
+      // to iterate over multiple generations of offset parents to find one that
+      // is scrollable.
+
+      while (el instanceof HTMLElement && el.offsetParent) {
+        offsetTop += el.offsetTop;
+        el = el.offsetParent;
+        if (window.getComputedStyle(el).overflowY === 'scroll') {
+          // Found scroll container. Note that we use CSS, rather than scrollTopMax,
+          // to avoid skipping over a container that might be scrollable but just
+          // happens to fit its content on-screen now.
+          container = el;
+          break;
+        }
+      }
       if (container) {
         container.scrollTop =
-          el.offsetTop + el.clientHeight / 2 - container.clientHeight / 2;
+          offsetTop + el.clientHeight / 2 - container.clientHeight / 2;
       }
     }
   }, [key, prevKey]);
