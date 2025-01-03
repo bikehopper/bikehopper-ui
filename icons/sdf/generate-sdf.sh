409b764bff1aca6c8e215ea0d8966afed9ae68de
Scott Feeney <scott@oceanbase.org>
2025-01-02 18:40:51 -0800

    Clean up script

--- icons/sdf/generate-sdf.sh
+++ icons/sdf/generate-sdf.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# The .svgs are from the @mdi/svg package with the attribute
-#   fill="#fff"
-# added to the <path>. (They must be rendered white on black.)
-#
 # This script requires ImageMagick to be installed, and also the
 # npm package image-sdf, globally.
 
@@ -16,10 +12,3 @@ for svg in *.svg; do
     >${i}_sdf.png && \
   rm ${i}_intermediate.png || echo Failed to convert $i
 done
-
-
-  # convert ${i}_intermediate.png -filter Jinc -resize 400% -threshold 30% \
-  #   \( +clone -negate -morphology Distance Euclidean -level 50%,-50% \) \
-  #   -morphology Distance Euclidean -compose Plus \
-  #   -composite -level 45%,55% -resize 25% \
-  #   ${i}_sdf.png && \
