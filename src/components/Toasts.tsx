f21618bdb38800bcda27e3fed3d22ef254f08433
Scott Feeney <scott@oceanbase.org>
2025-04-29 23:19:37 -0700

    Display BART elevator diagonals
    Importing the csv dependency causes TypeScript to think the return type
    of setTimeout is different for some reason.

--- src/components/Toasts.tsx
+++ src/components/Toasts.tsx
@@ -35,7 +35,9 @@ export default function Toasts() {
 
   // for some reason, radix's toast isn't automatically dismissing after the
   // passed duration like it should. so do it ourselves:
-  const dismissTimersRef = useRef<Map<number, number>>(new Map());
+  const dismissTimersRef = useRef<Map<number, ReturnType<typeof setTimeout>>>(
+    new Map(),
+  );
   useEffect(() => {
     const dismissTimers = dismissTimersRef.current;
     const alertIdsDisplayed = new Set();
